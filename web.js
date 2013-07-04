6afa93c7f9c56e58e87da60a6cd31c2b1acf4b4f
pobrob <rbpollock@brookspollock.com>
2013-07-04 21:57:58 +0000

    added correction

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFile('index.html' function (err, data){
+    fs.readFileSync('index.html' function (err, data){
 	if (err) thow err;
 	console.log(data);
 
